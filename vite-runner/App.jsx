6b4f9eef6a249009c98239ca01d38b7da20c7090
cxroop <chenxxyy299@gmail.com>
2024-06-10 23:06:01 +0800

    feat: 使用 jsx

---
+++ vite-runner/App.jsx
@@ -0,0 +1,6 @@
+import React from './core/React.js';
+
+
+const App = <div id='app'>Hello World 333</div>;
+console.log(App);
+export default App;
---
+++ vite-runner/main.jsx
@@ -0,0 +1,5 @@
+import App from './App';
+import React from './core/React.js';
+import ReactDOM from './core/ReactDom.js';
+
+ReactDOM.createRoot(document.querySelector('#root')).render(App);
